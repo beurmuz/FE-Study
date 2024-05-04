390c243be11c259eaaa84a7d026db490b279be4e
beurmuz <fallinta2@gmail.com>
2024-05-04 21:54:41 +0900

    #1 JavaScript basic 2

--- JavaScript-Study/basics/15_copy_by_value_and_reference.js
+++ JavaScript-Study/basics/15_copy_by_value_and_reference.js
@@ -1,96 +1,102 @@
 /**
  * NOTE: 아주 아주 중요한 개념!
  *
- * copy by value 값에 의한 전달
- * copy by reference 참조에 의한 전달
+ * copy by value 값에 의한 전달: (값을 그대로 복사)
+ * copy by reference 참조에 의한 전달: ()
  *
  * ! 1) 기본적으로 모든 primitive 값은 copy by value다
  * ! 2) 객체는 copy by reference다
  */
+
+// 1. copy by value
 let original = "안녕하세요";
 let clone = original;
 
-console.log(original);
-console.log(clone);
+console.log(original); // 안녕하세요
+console.log(clone); // 안녕하세요
 
-clone += " 안유진 입니다.";
+clone += " muse 입니다.";
 console.log("--------------");
-console.log(original);
-console.log(clone);
+console.log(original); // 안녕하세요
+console.log(clone); // 안녕하세요 muse 입니다.
 
+// 2. copy by reference
 let originalObj = {
-  name: "안유진",
-  group: "아이브",
+  name: "muse",
+  work: "LINE",
 };
-let cloneObj = originalObj;
+let cloneObj = originalObj; // 모두 같은 주소를 가리키고 있다.
 
-console.log(originalObj);
-console.log(cloneObj);
+console.log(originalObj); // { name: 'muse', work: 'LINE' }
+console.log(cloneObj); // { name: 'muse', work: 'LINE' }
 
 console.log("----------");
 
-originalObj["group"] = "코드팩토리";
-console.log(originalObj);
-console.log(cloneObj);
+// NOTE: original만 바꿔줬는데 clone도 함께 바뀌었다!
+originalObj["work"] = "TOSS"; // value 값 수정
+console.log(originalObj); // { name: 'muse', work: 'TOSS' }
+console.log(cloneObj); // { name: 'muse', work: 'TOSS' }
 
-console.log(originalObj === cloneObj);
-console.log(original === clone);
+console.log(originalObj === cloneObj); // ! true
+console.log(original === clone); // ! false
 
 originalObj = {
-  name: "최지호",
-  group: "코드팩토리",
+  name: "beurmuz",
+  work: "LINE",
 };
 cloneObj = {
-  name: "최지호",
-  group: "코드팩토리",
+  name: "beurmuz",
+  work: "LINE",
 };
-console.log(originalObj === cloneObj);
+console.log(originalObj === cloneObj); // false: 각 object의 값들이 같다고 해서 같은 obj인건 아니다.
 
-const yuJin1 = {
-  name: "안유진",
-  group: "아이브",
+console.log("---------------\n");
+console.log("문제 풀어보기");
+
+const muse1 = {
+  name: "muz",
+  work: "LINE",
 };
-const yuJin2 = yuJin1;
-const yuJin3 = {
-  name: "안유진",
-  group: "아이브",
+const muse2 = muse1; // muse1과 같은 주소값
+const muse3 = {
+  name: "muz",
+  work: "LINE",
 };
 
-// true
-console.log(yuJin1 === yuJin2);
-// false
-console.log(yuJin1 === yuJin3);
-// false
-console.log(yuJin2 === yuJin3);
+console.log(muse1 === muse2); // true
+console.log(muse1 === muse3); // false
+console.log(muse2 === muse3); // false
 
 /**
  * Spread Operator
+ * - spread operator는 아예 새로운 값을 return한다.
+ *   => 즉, spread operator는 ⭐️ copy by value ⭐️!
+ * - ⭐️⭐️⭐️ 순서에 따라 값이 변할 수 있다.
  */
-const yuJin4 = {
-  ...yuJin3,
+const muse4 = {
+  ...muse3,
 };
-console.log(yuJin4);
+console.log(muse4);
+console.log(muse4 === muse3); // false
 
-console.log(yuJin4 === yuJin3);
-
-const yuJin5 = {
-  year: 2003,
-  ...yuJin3,
+const muse5 = {
+  joinDate: 2024, // ⭐️ 새로운 key, value 추가
+  ...muse3,
 };
-console.log(yuJin5);
+console.log(muse5); // { joinDate: 2024, name: 'muz', work: 'LINE' }
 
-const yuJin6 = {
-  name: "코드팩토리",
-  ...yuJin3,
+const muse6 = {
+  name: "beurmuz", // ⭐️ spread 사용 전에 name을 썼으므로,
+  ...muse3, // muse3에 있는 name으로 이름이 바뀜
 };
-console.log(yuJin6);
+console.log(muse6); // { name: 'muz', work: 'LINE' }
 
-const yuJin7 = {
-  ...yuJin3,
-  name: "코드팩토리",
+const muse7 = {
+  ...muse3,
+  name: "beurmuz", // ⭐️ spread 후 이름을 변경했으므로 name이 바뀐다.
 };
-console.log(yuJin7);
+console.log(muse7); // { name: 'beurmuz', work: 'LINE' }
 
 const numbers = [1, 3, 5];
 const numbers2 = [...numbers, 10];
-console.log(numbers2);
+console.log(numbers2); // [1, 3, 5, 10]
--- JavaScript-Study/basics/16_try_catch.js
+++ JavaScript-Study/basics/16_try_catch.js
@@ -1,6 +1,8 @@
 /**
  * try...catch
  *
+ * 에러를 잡을 때 사용한다
+ *
  * 1) 발생시킬때 -> 던진다고한다. (throw)
  * 2) 명시적으로 인지할때 -> 잡는다고 한다. (catch)
  */
@@ -8,13 +10,15 @@ function runner() {
   try {
     console.log("Hello");
 
-    // throw new Error('큰 문제가 생겼습니다!');
+    // throw new Error("에러가 발생했습니다!"); // new: 새로운 객체를 생성함
 
-    console.log("Code Factory");
+    console.log("I'm beurmuz");
   } catch (e) {
+    // 에러를 잡는다
     console.log("---catch---");
     console.log(e);
   } finally {
+    // finally문은 항상 실행된다. 단, finally는 optional이다.
     console.log("---finally---");
   }
 }
